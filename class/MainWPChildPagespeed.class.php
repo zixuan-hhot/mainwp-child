0bf82f9049989698701ba6dfec4a959693ce4c1a
Thang Hoang Van <thang@mainwp.com>
2014-06-22 23:43:14 +0700

    Update for Page Speed extension

--- class/MainWPChildPagespeed.class.php
+++ class/MainWPChildPagespeed.class.php
@@ -26,9 +26,13 @@ class MainWPChildPagespeed
             switch ($_POST['mwp_action']) {                
                 case "save_settings":
                     $information = $this->save_settings();
+                    break;
                 case "set_showhide":
                     $information = $this->set_showhide();                    
-                break;
+                    break;
+                case "sync_data":
+                    $information = $this->sync_data();                    
+                    break;
             }        
         }
         MainWPHelper::write($information);
@@ -80,19 +84,7 @@ class MainWPChildPagespeed
         MainWPHelper::update_option('mainwp_pagespeed_ext_enabled', "Y");        
         $settings = $_POST['settings'];
         $settings = unserialize(base64_decode($settings));
-        
-//        $settings = array('api_key' => $api_key, 
-//                                'response_language' => $response_language, 
-//                                'report_type' => $report_type, 
-//                                'report_expiration' => $report_expiration,
-//                                'check_report' => $report_check,
-//                                'max_execution_time' => $exec_time, 
-//                                'delay_time' => $delay_time,
-//                                'log_exception' => $log_api_exception,
-//                                'scan_technical' => $scan_tech,
-//                                'delete_data' => $delete_data,
-//                               );
-        
+
         if (is_array($settings)) {
             $current_values = get_option('gpagespeedi_options');
             
@@ -102,36 +94,145 @@ class MainWPChildPagespeed
             if (isset($settings['response_language']))                
                 $current_values['response_language'] = $settings['response_language'];
             
+            if (isset($settings['report_type']))                
+                $current_values['strategy'] = $settings['report_type'];
+                        
             if (isset($settings['max_execution_time']))                
                 $current_values['max_execution_time'] = $settings['max_execution_time'];
             
             if (isset($settings['delay_time']))                
                 $current_values['sleep_time'] = $settings['delay_time'];    
             
-//                'sleep_time'                => $sleep_time,
-//                'log_api_errors'            => $log_api_errors,
-//                'scan_method'               => $scan_method,
-//                'recheck_interval'          => $recheck_interval,
-//                'check_pages'               => $check_pages,
-//                'check_posts'               => $check_posts,
-//                'cpt_whitelist'             => $cpt_whitelist,
-//                'check_categories'          => $check_categories,
-//                'first_run_complete'        => $options['first_run_complete'],
-//                'last_run_finished'         => $options['last_run_finished'],
-//                'bad_api_key'               => false,
-//                'pagespeed_disabled'        => false,
-//                'new_ignored_items'         => false,
-//                'backend_error'             => false,
-//                'new_activation_message'    => false
+            if (isset($settings['log_exception']))                
+                $current_values['log_api_errors'] = ($settings['log_exception']) ? true : false;  
             
+            if (isset($settings['scan_technical']))                
+                $current_values['scan_method'] = $settings['scan_technical'];  
+            
+            if (isset($settings['report_expiration']))                
+                $current_values['recheck_interval'] = $settings['report_expiration'];  
+            
+            if (isset($settings['check_report'])) {
+                if (is_array($settings['check_report'])) {                            
+                    $current_values['check_pages'] = in_array('page', $settings['check_report']) ? true : false;      
+                    $current_values['check_posts'] = in_array('post', $settings['check_report']) ? true : false;      
+                    $current_values['check_categories'] = in_array('category', $settings['check_report']) ? true : false;      
+                } else {
+                    $current_values['check_pages'] = $current_values['check_posts'] = $current_values['check_categories'] = false;
+                }
+            }
+          
+            if (isset($settings['delete_data']) && !empty($settings['delete_data'])) {
+                $this->delete_data($settings['delete_data']);
+            }
+  
             if (update_option( 'gpagespeedi_options', $current_values ))
                 $information['result'] = 'SUCCESS';
             else 
-                $information['result'] = 'NOTCHANGE';
-        }
-        
+                $information['result'] = 'NOTCHANGE';           
+        }        
+        $sync_data = $this->sync_data();        
+        $information['data'] = $sync_data['data'];        
         return $information;
     }
     
+    function delete_data($what) {
+        global $wpdb;
+        $gpi_page_stats = $wpdb->prefix . 'gpi_page_stats';
+        $gpi_page_reports = $wpdb->prefix . 'gpi_page_reports';
+        $gpi_page_blacklist = $wpdb->prefix . 'gpi_page_blacklist';
+
+        if($what == 'purge_reports') {
+            $wpdb->query("TRUNCATE TABLE $gpi_page_stats");
+            $wpdb->query("TRUNCATE TABLE $gpi_page_reports");
+        } elseif($what == 'purge_everything') {
+            $wpdb->query("TRUNCATE TABLE $gpi_page_stats");
+            $wpdb->query("TRUNCATE TABLE $gpi_page_reports");
+            $wpdb->query("TRUNCATE TABLE $gpi_page_blacklist");
+        }
+    }   
+    
+    function sync_data() {
+        $current_values = get_option('gpagespeedi_options');
+        $information = array();
+        $data = array('bad_api_key' => $current_values['bad_api_key']);        
+        $data['average_score'] = self::cal_average_score();
+        $information['data'] = $data;
+        return $information;
+    }
+    
+    static function cal_average_score() {
+        global $wpdb;
+        
+        if ( !defined('GPI_DIRECTORY') )
+            return 0;
+        
+        require_once( GPI_DIRECTORY . '/includes/helper.php' );
+        
+        $options = get_option('gpagespeedi_options');
+        
+        $strategy = ( isset($options['strategy']) ) ? $options['strategy'] : 'desktop';
+        $score_column = $strategy . '_score';
+        $page_stats_column = $strategy . '_page_stats';        
+        require_once( ABSPATH . 'wp-admin/includes/template.php' );
+        require_once( GPI_DIRECTORY . '/core/init.php' );      
+        $GPI_ListTable = new GPI_List_Table();
+        
+        $data_typestocheck = $GPI_ListTable->getTypesToCheck('all');
+        
+        $gpi_page_stats = $wpdb->prefix . 'gpi_page_stats';
+        if(!empty($data_typestocheck)) {
+
+            $allpagedata =  $wpdb->get_results(
+                                $wpdb->prepare(
+                                    "SELECT ID, URL, $score_column, $page_stats_column
+                                    FROM $gpi_page_stats
+                                    WHERE ($data_typestocheck[0])",
+                                    $data_typestocheck[1]
+                                ),
+                                ARRAY_A 
+                            );
+        } else {
+            $allpagedata = array();
+        }
+
+        $reports_typestocheck = $GPI_ListTable->getTypesToCheck('all');
+        $gpi_page_reports = $wpdb->prefix . 'gpi_page_reports';
+
+        if(!empty($reports_typestocheck)) {
+
+            $allpagereports =   $wpdb->get_results(
+                                    $wpdb->prepare( 
+                                        "SELECT     r.rule_key, r.rule_name, r.rule_impact
+                                        FROM        $gpi_page_stats d
+                                        INNER JOIN  $gpi_page_reports r 
+                                                    ON r.page_id = d.ID
+                                                    AND r.rule_impact > 0
+                                                    AND r.strategy = '$strategy'
+                                        WHERE       ($reports_typestocheck[0])",
+                                        $reports_typestocheck[1]
+                                    ),
+                                    ARRAY_A
+                                ); 
+        } else {
+            $allpagereports = array();
+        }
+        
+        $total_pages = count($allpagedata);
+        $total_scores = 0;
+        $average_score = 0;
+        
+        if(!empty($total_pages) && !empty($allpagereports)) {
+
+            foreach($allpagedata as $key => $pagedata)
+            {
+                $total_scores = $total_scores + $pagedata[$score_column];
+            }
+
+            $average_score = number_format($total_scores / $total_pages);
+        }
+        
+        return $average_score;
+    }
 }
 
