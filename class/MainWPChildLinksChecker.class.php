46f71456c1b3236c1188816a642601c05e4e9553
Thang Hoang Van <thang@mainwp.com>
2014-07-22 23:29:49 +0700

    Update for Broken Links Checker extension

--- class/MainWPChildLinksChecker.class.php
+++ class/MainWPChildLinksChecker.class.php
@@ -80,20 +80,22 @@ class MainWPChildLinksChecker
     
     function sync_data($strategy = "") {  
         $information = array();           
-        if (!defined('BLC_ACTIVE')) {
+        if (!defined('BLC_ACTIVE') && !function_exists('blc_init')) {
             $information['error'] = 'NO_BROKENLINKSCHECKER';
             MainWPHelper::write($information);
-        }                                
+        }     
+        blc_init();
         $data = array();
-        $data['broken'] = self::get_sync_data('broken');
-        $data['redirects'] = self::get_sync_data('redirects');
-        $data['dismissed'] = self::get_sync_data('dismissed');
-        $data['all'] = self::get_sync_data('all');        
+        $data['broken'] = self::sync_counting_data('broken');
+        $data['redirects'] = self::sync_counting_data('redirects');
+        $data['dismissed'] = self::sync_counting_data('dismissed');
+        $data['all'] = self::sync_counting_data('all');  
+        $data['link_data'] = self::sync_link_data();          
         $information['data'] = $data;
         return $information;
     }
     
-    static function get_sync_data($filter) {       
+    static function sync_counting_data($filter) {       
         global $wpdb;
         
         $all_filters = array(
@@ -107,15 +109,112 @@ class MainWPChildLinksChecker
         if (empty($where))
             return 0;
         
-        $q = "  SELECT COUNT(*)
-                FROM (	SELECT 0
-                        FROM 
-                        {$wpdb->prefix}blc_links AS links                         
-                        WHERE $where
-                GROUP BY links.link_id) AS foo";
-
-        return $wpdb->get_var($q);       
+        return blc_get_links(array('count_only' => true, 'where_expr' => $where));
     }
+    
+    static function sync_link_data() {        
+        $links = blc_get_links(array('load_instances' => true));
+        $get_fields = array(
+            'link_id',
+            'url',
+            'being_checked',
+            'last_check',
+            'last_check_attempt',
+            'check_count',
+            'http_code',
+            'request_duration',
+            'timeout',
+            'redirect_count',
+            'final_url',
+            'broken', 
+            'first_failure',
+            'last_success',
+            'may_recheck',
+            'false_positive',
+            //'result_hash',
+            'dismissed', 
+            'status_text',
+            'status_code',
+            'log',
+        );
+        $return = "";
+        $site_id = $_POST['site_id'];
+        $blc_option = get_option('wsblc_options');
+        if (is_array($links)) {
+            foreach($links as $link) {
+                $lnk = new stdClass();
+                foreach($get_fields as $field) {
+                    $lnk->$field = $link->$field;
+                }
+                
+                if (!empty($link->post_date) ) {
+                    $lnk->post_date = $link->post_date;   
+                } 
+                
+                $days_broken = 0;
+                if ( $link->broken ){
+                        //Add a highlight to broken links that appear to be permanently broken
+                        $days_broken = intval( (time() - $link->first_failure) / (3600*24) );
+                        if ( $days_broken >= $blc_option['failure_duration_threshold'] ){
+                                $lnk->permanently_broken = 1;
+                                if ( $blc_option['highlight_permanent_failures'] ){
+                                    $lnk->permanently_broken_highlight = 1;
+                                }
+                        }
+                }
+                $lnk->days_broken = $days_broken;
+                if ( !empty($link->_instances) ){			
+                    $instance = reset($link->_instances); 
+                    $lnk->link_text = $instance->ui_get_link_text();                    
+                    $lnk->count_instance = count($link->_instances);                    
+                    $container = $instance->get_container(); /** @var blcContainer $container */
+                    $lnk->container = $container;
+                    
+                    if ( !empty($container) && ($container instanceof blcAnyPostContainer) ) {                        
+                        $lnk->container_type = $container->container_type;
+                        $lnk->container_id = $container->container_id;
+                    }
+                    
+                    $can_edit_text = false;
+                    $can_edit_url = false;
+                    $editable_link_texts = $non_editable_link_texts = array();
+                    $instances = $link->_instances;
+                    foreach($instances as $instance) {
+                            if ( $instance->is_link_text_editable() ) {
+                                    $can_edit_text = true;
+                                    $editable_link_texts[$instance->link_text] = true;
+                            } else {
+                                    $non_editable_link_texts[$instance->link_text] = true;
+                            }
 
+                            if ( $instance->is_url_editable() ) {
+                                    $can_edit_url = true;
+                            }
+                    }
+
+                    $link_texts = $can_edit_text ? $editable_link_texts : $non_editable_link_texts;
+                    $data_link_text = '';
+                    if ( count($link_texts) === 1 ) {
+                            //All instances have the same text - use it.
+                            $link_text = key($link_texts);
+                            $data_link_text = esc_attr($link_text);
+                    }
+                    $lnk->data_link_text =  $data_link_text;
+                    $lnk->can_edit_url =  $can_edit_url;
+                    $lnk->can_edit_text =  $can_edit_text;                    
+		} else {
+                    $lnk->link_text = "";
+                    $lnk->count_instance = 0;
+                }                
+                $lnk->site_id = $site_id; 
+                                
+                $return[] = $lnk;            
+            }
+        } else 
+            return "";
+        
+        return $return;
+  
+    }   
 }
 
