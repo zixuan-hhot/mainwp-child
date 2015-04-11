893b5396e5dd0ddd1426e6687267e6c9b7ce83c7
Thang Hoang Van <thang@mainwp.com>
2015-04-11 22:00:27 +0700

    Fix bug for restore backup of updraftplug extension

--- class/MainWPChild.class.php
+++ class/MainWPChild.class.php
@@ -67,7 +67,7 @@ class MainWPChild
         'update_values' => 'update_values',
         'ithemes' => 'ithemes',
         'multistores' => 'multistores',
-        'updraftplus' => 'updraftplus'
+        'updraftplus' => 'updraftplus'        
     );
 
     private $FTP_ERROR = 'Failed, please add FTP details for automatic upgrades.';
@@ -791,8 +791,8 @@ class MainWPChild
         new MainWPChildIThemesSecurity();
 
         MainWPChildWooCommerceMultiStores::Instance()->init();
-        MainWPChildUpdraftplusBackups::Instance()->updraftplus_init();
-
+        MainWPChildUpdraftplusBackups::Instance()->updraftplus_init();                    
+      
         //Call the function required
         if (isset($_POST['function']) && isset($this->callableFunctions[$_POST['function']]))
         {
