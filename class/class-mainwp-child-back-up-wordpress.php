e5187b5a686b18b0f4041a1874f06405e3223b02
Thang Hoang Van <thang@mainwp.com>
2016-03-22 00:53:19 +0700

    Fixed: to support new version of BackupWordpress plugin

--- class/class-mainwp-child-back-up-wordpress.php
+++ class/class-mainwp-child-back-up-wordpress.php
@@ -569,9 +569,9 @@ class MainWP_Child_Back_Up_Wordpress {
 				$site_size = new HM\BackUpWordPress\Site_Size;
 
 				$exclude_string = implode( '|', $excludes->get_excludes_for_regex() );
-
+				
 				if (function_exists('HM\BackUpWordPress\list_directory_by_total_filesize'))
-					$files = HM\BackUpWordPress\list_directory_by_total_filesize( $directory );
+					$files = HM\BackUpWordPress\list_directory_by_total_filesize( $directory, $excludes );
 
 			} else {
 				$files = $schedule->list_directory_by_total_filesize( $directory );
