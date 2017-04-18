4d860a7e012bdfa5988caf1431ef34e6fec6b434
Thang Hoang Van <thang@mainwp.com>
2017-04-18 14:48:39 +0700

    Added: support new settings for wordfence Fixed: bug for itheme saving settings Fixed: bug empty and incorrect version number of update plugins/themes in client reports Fixed: bug creating custom post type

--- class/class-mainwp-child-ithemes-security.php
+++ class/class-mainwp-child-ithemes-security.php
@@ -174,7 +174,7 @@ class MainWP_Child_iThemes_Security {
 		$nbf_settings = array();
 		
 		$update_settings         = maybe_unserialize( base64_decode( $_POST['settings'] ) );
-						
+		
 		foreach($update_settings as $module => $settings) {
 			$do_not_save = false;
 			if (in_array($module, $_itsec_modules)) {	
@@ -241,7 +241,7 @@ class MainWP_Child_iThemes_Security {
 					}					
 					$settings = $nbf_settings;
 				} 			
-				if ( !$do_not_save ) {
+				if ( !$do_not_save ) {                                    
 					ITSEC_Modules::set_settings( $module, $settings );
 					$updated          = true;
 				}
@@ -1074,7 +1074,7 @@ class MainWP_Child_iThemes_Security {
 		);
 
 		if ( $all_sites ) {
-			$query = 'SHOW_TABLES';
+			$query = 'SHOW TABLES';
 		} else {
 			$query = $wpdb->prepare( 'SHOW TABLES LIKE %s', "{$wpdb->base_prefix}%" );
 		}
