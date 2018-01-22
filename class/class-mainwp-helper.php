8164df70113caffbf85f46a0e9866b3e436251f7
Thang Hoang Van <thang@mainwp.com>
2018-01-22 15:53:17 +0700

    Fixed: warning

--- class/class-mainwp-helper.php
+++ class/class-mainwp-helper.php
@@ -1281,23 +1281,29 @@ class MainWP_Helper {
 	 * Credit to the : wp-filters-extras
 	 */
 
-	public static function remove_filters_with_method_name( $hook_name = '', $method_name = '', $priority = 0 ) {
-		global $wp_filter;
-		// Take only filters on right hook name and priority
-		if ( ! isset( $wp_filter[ $hook_name ] ) || ! isset( $wp_filter[ $hook_name ][ $priority ] ) || ! is_array( $wp_filter[ $hook_name ][ $priority ] ) ) {
-			return false;
-		}
-
-		// Loop on filters registered
-		foreach ( (array) $wp_filter[ $hook_name ][ $priority ] as $unique_id => $filter_array ) {
-			// Test if filter is an array ! (always for class/method)
-			if ( isset( $filter_array['function'] ) && $filter_array['function'] === $method_name ) {
-				unset( $wp_filter[ $hook_name ][ $priority ][ $unique_id ] );
-			}
-		}
-
+function remove_filters_with_method_name( $hook_name = '', $method_name = '', $priority = 0 ) {
+	global $wp_filter;
+	// Take only filters on right hook name and priority
+	if ( ! isset( $wp_filter[ $hook_name ][ $priority ] ) || ! is_array( $wp_filter[ $hook_name ][ $priority ] ) ) {
 		return false;
 	}
+	// Loop on filters registered
+	foreach ( (array) $wp_filter[ $hook_name ][ $priority ] as $unique_id => $filter_array ) {
+		// Test if filter is an array ! (always for class/method)
+		if ( isset( $filter_array['function'] ) && is_array( $filter_array['function'] ) ) {
+			// Test if object is a class and method is equal to param !
+			if ( is_object( $filter_array['function'][0] ) && get_class( $filter_array['function'][0] ) && $filter_array['function'][1] == $method_name ) {
+				// Test for WordPress >= 4.7 WP_Hook class 
+				if ( is_a( $wp_filter[ $hook_name ], 'WP_Hook' ) ) {
+					unset( $wp_filter[ $hook_name ]->callbacks[ $priority ][ $unique_id ] );
+				} else {
+					unset( $wp_filter[ $hook_name ][ $priority ][ $unique_id ] );
+				}
+			}
+		}
+	}
+	return false;
+}
 
 	public static function sanitize_filename( $filename ) {
 		if (!function_exists('mb_ereg_replace')) return sanitize_file_name($filename);
